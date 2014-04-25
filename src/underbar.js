da005ca2e7cb80c01e859c9a1b8c6e1f791ee8a3
Jasen Lew <jasen.lew@gmail.com>
2014-04-25 15:08:00 -0700

    Fixed each function

--- src/underbar.js
+++ src/underbar.js
@@ -42,7 +42,7 @@ var _ = {};
     if (n > array.length) {
       n = array.length; 
     }
-    
+
     return n === undefined ? array[array.length - 1] : array.slice(array.length - n, array.length);
   };
 
@@ -52,6 +52,17 @@ var _ = {};
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if (Array.isArray(collection)) {
+      for (var i = 0, l = collection.length; i < l; i += 1) {
+        iterator(collection[i], i, collection);
+      }
+    } else if (typeof(collection) === 'object') {
+      for (var key in collection) {
+        if (collection.hasOwnProperty(key)) {
+          iterator(collection[key], key, collection);
+        }
+      }
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
