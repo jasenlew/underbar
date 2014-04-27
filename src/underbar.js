a87971ccdd46cbb2092ecd2af4cfb9a4d6c0e481
Jasen Lew <jasen.lew@gmail.com>
2014-04-26 21:48:36 -0700

    (In-progress) Fixed _.invoke function

--- src/underbar.js
+++ src/underbar.js
@@ -180,6 +180,15 @@ var _ = {};
   // Calls the method named by methodName on each value in the list.
   // Note: you will nead to learn a bit about .apply to complete this.
   _.invoke = function(collection, functionOrKey, args) {
+
+    var result = [];
+  
+    _.each(collection, function (item) {
+      result.push(functionOrKey.apply(item));
+    });
+
+    return result;
+
   };
 
   // Reduces an array or object to a single value by repetitively calling
