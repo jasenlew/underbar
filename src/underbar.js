446d8da3a61cfeff40f4305e96dfff2cd3a6dd5a
Jasen Lew <jasen.lew@gmail.com>
2014-04-26 17:46:12 -0700

    Fixed _.map function

--- src/underbar.js
+++ src/underbar.js
@@ -148,6 +148,15 @@ var _ = {};
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+
+    var arr = [];
+
+    _.each(collection, function (params) {
+      arr.push(iterator(params));
+    });
+
+    return arr;
+
   };
 
   /*
